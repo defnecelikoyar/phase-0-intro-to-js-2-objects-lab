e5bba558c57d89a2a45075dc1f6fe1698d711d1e
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-24 16:53:01 -0500

    commit tree 719e1c32a0e0fd23f4e3bc59bc22090ffc60bf5c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'defne',
+    streetAddress: '368 eastern pkwy',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj2 = {...employee[key]};
+    delete newObj2[key];
+    return newObj2;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
+
